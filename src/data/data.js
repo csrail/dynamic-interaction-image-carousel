0162742e8fa6d5c9b9abbc80ba4d373b056b3049
Cornelius Soon <cornelius.soon@gmail.com>
2023-12-07 13:13:00 +1300

    Add pillar image to data file

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,5 @@
 import globe from "../images/globe.jpg";
+import pillar from "../images/pillar.jpg";
 import helperMixin from "../javascript/helperFunctions";
 
 const imageCarouselItemData1 = {
@@ -9,6 +10,12 @@ const imageCarouselItemData1 = {
     imageWidth: 600,
 };
 
-const imageCarouselItemData2 = {};
+const imageCarouselItemData2 = {
+    imageSource: helperMixin.createImage(pillar),
+    imageName: "Pillar",
+    altName: "Three pillars make a temple.",
+    imageHeight: 600,
+    imageWidth: 600,
+};
 
 export { imageCarouselItemData1, imageCarouselItemData2 };

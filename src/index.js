8ba83c0a8c68ae22d7a55b05d5c121d643124e0d
Cornelius Soon <cornelius.soon@gmail.com>
2023-12-08 09:45:50 +1300

    Refactor to remove variable assignments

--- src/index.js
+++ src/index.js
@@ -5,17 +5,13 @@ import ImageCarouselItemViews from "./javascript/imageCarouselItemViews";
 import ImageCarouselView from "./javascript/imageCarouselView";
 
 (() => {
-    const imageCarouselItemView = ImageCarouselItemView();
-    const imageCarouselItem1 = ImageCarouselItem(imageCarouselItemData1);
-    const imageCarouselItem2 = ImageCarouselItem(imageCarouselItemData2);
-
-    const imageCarouselItemViews = ImageCarouselItemViews(
-        imageCarouselItemView,
-        imageCarouselItem1,
-        imageCarouselItem2,
+    const imageCarouselView = ImageCarouselView(
+        ImageCarouselItemViews(
+            ImageCarouselItemView(),
+            ImageCarouselItem(imageCarouselItemData1),
+            ImageCarouselItem(imageCarouselItemData2),
+        ),
     );
-
-    const imageCarouselView = ImageCarouselView(imageCarouselItemViews);
 
     document.body.appendChild(imageCarouselView.displayView());
 

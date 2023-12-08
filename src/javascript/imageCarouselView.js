f142f5c3cbc012926ff172291aeedf4126254030
Cornelius Soon <cornelius.soon@gmail.com>
2023-12-09 11:29:33 +1300

    Add right arrow, next image feature

--- src/javascript/imageCarouselView.js
+++ src/javascript/imageCarouselView.js
@@ -1,5 +1,6 @@
 const ImageCarouselView = (imageCarouselItemViewsObject = {}) => {
     const imageCarouselItemViews = imageCarouselItemViewsObject;
+    let indexPosition = 0;
 
     const buildImageCarousel = (views) => {
         const component = document.createElement("div");
@@ -10,8 +11,6 @@ const ImageCarouselView = (imageCarouselItemViewsObject = {}) => {
     };
 
     const processImages = (imageElements) => {
-        let indexPosition;
-
         imageElements.forEach((element, index) => {
             if (element.classList.contains("visible")) {
                 element.classList.remove("visible");
@@ -27,6 +26,32 @@ const ImageCarouselView = (imageCarouselItemViewsObject = {}) => {
         });
     };
 
+    const buildRightArrow = () => {
+        const component = document.createElement("div");
+        const arrowElement = document.createElement("div");
+        arrowElement.classList.add(".right-arrow");
+        arrowElement.textContent = ">";
+
+        arrowElement.addEventListener("click", () => {
+            const imageCarousel = [...document.querySelectorAll("img")];
+            const visibleImage = document.querySelector(
+                "img[class~='visible']",
+            );
+            visibleImage.classList.remove("visible");
+            if (indexPosition + 1 === imageCarousel.length) {
+                indexPosition = 0;
+            } else {
+                indexPosition += 1;
+            }
+
+            imageCarousel[indexPosition].classList.add("visible");
+        });
+
+        component.appendChild(arrowElement);
+
+        return component;
+    };
+
     const buildImageCarouselComponent = () => {
         const container = document.createElement("div");
         const imageCarouselMainContainer = document.createElement("div");
@@ -34,7 +59,7 @@ const ImageCarouselView = (imageCarouselItemViewsObject = {}) => {
         const imageCarousel = buildImageCarousel(imageCarouselItemViews);
 
         const imageCarouselLeftArrowContainer = document.createElement("div");
-        const imageCarouselRightArrowContainer = document.createElement("div");
+        const imageCarouselRightArrowContainer = buildRightArrow();
 
         const imageCarouselSecondaryContainer = document.createElement("div");
         const imageCarouselDots = document.createElement("div");
